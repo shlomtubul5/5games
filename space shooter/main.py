058457e6a1cceeedf65a68ddeef42130935d5102
shalom <shalomtubul30@gmail.com>
2024-09-16 09:21:02 +0300

    importing an image

--- space shooter/main.py
+++ space shooter/main.py
@@ -1,24 +1,30 @@
-import pygame
+import pygame as pg
 
 # general setup
-pygame.init()
+pg.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
-display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+display_surface = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 running = True
 
-
-surf =  pygame.Surface((100,100))
+# plain surf
+x = 0
+surf = pg.Surface((100, 100))
 surf.fill("orange")
+
+# importing an image
+player_surf = pg.image.load("images/player.png")
+
 
 while running:
     # event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
     display_surface.fill((255, 0, 0))
     # drow the game
-    display_surface.blit(surf,(0,0))
-    pygame.display.update()
+    x += 0.1
+    display_surface.blit(surf, (x, 0))
+    pg.display.update()
 
-pygame.quit()
+pg.quit()
